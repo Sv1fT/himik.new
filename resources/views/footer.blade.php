708ee2ad722de5d83fb079d7e6f89247fc7389f0
Sv1fT <sv1ft.one@gmail.com>
2019-12-03 01:39:28 +0400

    add footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="page-footer font-small bg-dark">
+
+    <!-- Copyright -->
+    <div class="footer-copyright py-3 text-center text-white">© 2019
+        <a class="text-white" href="{{ route('home') }}"> ОПТхимик</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->

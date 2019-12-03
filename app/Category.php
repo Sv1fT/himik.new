7ce77bf961e99bc91ee80768a1a3a2d125f506e5
Dmitrij <oson-dmitrij@Yandex.ru>
2019-12-03 15:13:51 +0400

    add models and controllers

---
+++ app/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    public function subcategories()
+    {
+        return $this->hasMany(Subcategory::class ,'category_id');
+    }
+
+    public function advert()
+    {
+        return $this->hasMany(Advert::class,'category');
+    }
+}

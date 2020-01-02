ad30867a7b18220d2de4768cf92d8b0b50acbfd5
Sv1fT <osonow31@icloud.com>
2020-01-02 13:23:33 +0400

    Карусель компаний

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,18 +7,7 @@
 
             <div class="owl-carousel text-center d-block col-md-12">
                 @foreach($companies as $key => $company)
-                    @if(Illuminate\Support\Facades\Storage::disk('public')->exists($company->attributes->filename))
-                        @if($company->attributes->site != "")
-                            <a href="http://{{$company->attributes->site}}" target="_blank" rel="noreferrer">
-                                <img class="h-50 my-3" data-src="/storage/{{$company->attributes->filename}}" title="{{$company->attributes->title}}"
-                                     alt="{{$company->attributes->company}}"></a>@else
-                            <a href="/">
-                                <img class="h-50 my-3" src="/storage/{{$company->attributes->filename}}" rel="noreferrer" title="{{$company->attributes->title}}"
-                                     alt="{{$company->attributes->company}}"></a>
-                        @endif
-                    @else
-                        <img class="img-fluid h-50 my-3" src="https://image.freepik.com/free-vector/error-404-found-glitch-effect_8024-4.jpg" title="{{$company->title}}" alt="{{$company->company}}">
-                    @endif
+                    <img class="h-50 my-3" src="{{ Storage::disk('public')->exists($company->attributes->filename) ? asset('storage/' . $company->attributes->filename) : "https://image.freepik.com/free-vector/error-404-found-glitch-effect_8024-4.jpg" }}" rel="noreferrer" title="{{$company->attributes->title}}" alt="{{$company->attributes->company}}"></a>
                 @endforeach
             </div>
         </div>
